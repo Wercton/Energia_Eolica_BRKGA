bfccc383bed8511f125f6157781d532751c87900
Wercton <werctonmatheus@gmail.com>
2020-08-05 18:43:23 -0300

    roda ai tio

--- Main.py
+++ Main.py
@@ -4,9 +4,12 @@ import numpy as np
 import Populacao
 import Selecao
 import Reproducao
+from random import randrange
 
 plt.style.use('bmh')
 warnings.filterwarnings('ignore', 'The iteration is not making good progress')
+
+code = 0
 
 
 def brkga(individuos, genes, geracao, mutantes_quantidade):
@@ -19,8 +22,6 @@ def brkga(individuos, genes, geracao, mutantes_quantidade):
         populacao = Populacao.criar_populacao(individuos, genes, nova=True)
 
         for geracao_atual in range(geracao):
-
-            print(".")
 
             populacao = np.squeeze(np.asanyarray(populacao))  # transforma população em array numpy
             fitness_populacao = Populacao.geral_fitness(populacao)
@@ -39,7 +40,8 @@ def brkga(individuos, genes, geracao, mutantes_quantidade):
 
                 populacao.append(Reproducao.produzir_filho(elite, nao_elite))
 
-            exibir_dados(geracao_atual, melhor_solucao_geracao)
+            # exibir_dados(geracao_atual, melhor_solucao_geracao)
+            print(geracao_atual, end=" ")
 
             if melhor_solucao_geracao[1] > melhor_solucao_geral[1]:
                 melhor_solucao_geral = melhor_solucao_geracao
@@ -47,13 +49,9 @@ def brkga(individuos, genes, geracao, mutantes_quantidade):
             melhores_solucoes.append(melhor_solucao_geral[1])
 
     print("\n\nFitness do melhor indíviduo encontrado:", melhor_solucao_geral[1])
-    print("Indivíduo: ", [i for i in melhor_solucao_geral[0]], sep="\n")
+    print("Indivíduo: ", [i for i in melhor_solucao_geral[0]], sep="\n\n")
 
-    plt.plot([i for i in range(len(melhores_solucoes))], melhores_solucoes)
-    fig1 = plt.gcf()
-    plt.show()
-    plt.draw()
-    fig1.savefig('200geracoes.png', transparent=True)
+    plottar_grafico(melhores_solucoes, individuos, mutantes_quantidade)
 
 
 def exibir_dados(geracao, melhor_solucao_geracao):
@@ -61,7 +59,27 @@ def exibir_dados(geracao, melhor_solucao_geracao):
     print("Melhor solução:", melhor_solucao_geracao[1])
 
 
-if __name__ == '__main__':
+def plottar_grafico(melhores_solucoes, individuos, mutantes_quantidade):
 
-        brkga(individuos=50, genes=50, geracao=100, mutantes_quantidade=10)
+    global code
+
+    plt.plot([i for i in range(1, len(melhores_solucoes) + 1)], melhores_solucoes)
+    plt.xlabel("Número de gerações")
+    plt.ylabel("Potência (W)")
+    plt.title("Curva de Convergência - BRKGA")
+
+    fig1 = plt.gcf()
+    # plt.show()
+    plt.draw()
+    nome_arquivo = "graficos/conv" + str(individuos) + "indiv" + str(mutantes_quantidade) + "mut" + "_code" + str(code)
+    fig1.savefig(nome_arquivo + "tran", transparent=True)
+    fig1.savefig(nome_arquivo)
+    plt.cla()
+
+    code += 1
+
+
+if __name__ == '__main__':
+    for _ in range(100):
+        brkga(individuos=10, genes=50, geracao=5, mutantes_quantidade=1)
 
